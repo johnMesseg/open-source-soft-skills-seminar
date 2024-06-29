0a5313979577c8ee13e8933b1cf36f485e7eb522
SuperDedo <guy.bornstein@gmail.com>
2024-06-29 20:14:58 +0300

    classes student and Course

---
+++ Course.py
@@ -0,0 +1,25 @@
+class Course:
+
+    def __init__(self, name, teacher):
+        pass
+
+    # adds the student and his grade to the course
+    # need to also add the course to the studant object
+    def addStudent(self, student, grade):
+        pass
+
+    #returns the average of the course
+    def getAverage(self):
+        pass
+
+    #returns the median of the course
+    def getMedian(self):
+        pass
+
+    #returns the prectenge of falis in this class
+    def getFailPrecentage(self):
+        pass
+
+    #gets int k and returns the k largest grade in the course.
+    def getKgrade(self, k):
+        pass
